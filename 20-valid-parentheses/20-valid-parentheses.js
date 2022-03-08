2e180a9681c848fb51eee423b7e552a80fbca8e5
ENGK0007 <79755492+ENGK0007@users.noreply.github.com>
2022-03-08 23:10:48 +0800

    Time: 89 ms (53.78%), Space: 42.4 MB (34.29%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -0,0 +1,36 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    
+    
+    
+    stack = [];
+    for(i = 0 ; i < s.length ; i++){
+        
+        
+        if( s[i] == '(' || s[i] == '{' || s[i] == '[')
+            stack.push(s[i]);
+        
+        else if (s[i] == ')'){
+            if(stack.pop() != '(')
+                return false
+        }
+            
+        else if (s[i] == ']'){
+            if(stack.pop() != '[')
+                return false
+        }
+        else if (s[i] == '}'){
+            if(stack.pop() != '{')
+                return false
+        }
+            
+    }
+    if (stack.length > 0)
+        return false
+    
+    return true;
+    
+};
