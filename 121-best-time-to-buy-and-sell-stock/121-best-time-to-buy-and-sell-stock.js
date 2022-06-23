d57fdd4b1f2fe35a447e13aad56a73703c152637
ENGK0007 <79755492+ENGK0007@users.noreply.github.com>
2022-06-24 00:39:31 +0800

    Time: 137 ms (28.55%), Space: 51.8 MB (53.45%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -5,13 +5,13 @@
 var maxProfit = function(prices) {
     
     
-    min = Number.MAX_SAFE_INTEGER;
-    max = 0;
+    min = Number.MAX_SAFE_INTEGER; // lowest price-> best time to buy
+    max = 0; // maximum profit
     
     for(i = 0; i < prices.length; i++){
-        min = Math.min(min, prices[i]);
-        max = Math.max(max, prices[i] - min);
         
+        min = Math.min(min, prices[i]); // check if the stock price if lower, use the lower price -> we buy at lower price
+        max = Math.max(max, prices[i] - min); // calculate profit and choose the max profit
         
         
     }  
