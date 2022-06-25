9bd6e552b02f11e80ba7779dba1a2014329444d2
ENGK0007 <79755492+ENGK0007@users.noreply.github.com>
2022-06-26 00:25:34 +0800

    Time: 122 ms (9.52%), Space: 45.5 MB (9.77%) - LeetHub

---
+++ 704-binary-search/704-binary-search.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    
+    
+    let l = 0, r = nums.length - 1;
+
+    while(l <= r){
+        m = Math.floor( l + (r-l)/2 );
+        
+        if(target == nums[m])
+            return m;
+        else if (nums[m] > target)
+            r = m - 1;
+        else
+            l = m + 1;
+        
+    }
+ 
+    return -1;
+};
